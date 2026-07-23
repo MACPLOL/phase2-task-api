70f405725584c0eabf2cf52973be184965740105
MACPLOL <maurocp277@gmail.com>
2026-07-23 13:51:26 -0400

    Add user model and database migration

--- models.py
+++ models.py
@@ -16,3 +16,16 @@ class Task(Base):
     text: Mapped[str] = mapped_column(String(255))
     completed: Mapped[bool] = mapped_column(default=False)
     priority: Mapped[str | None] = mapped_column(String(20), nullable=True)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    #the id is an int and primary key
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    #the email is a required unique string
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+    #hashed password is a string and is required to be inputted.
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
